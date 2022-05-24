014f58eaa3686905df01e442706991d42feb8623
main <oleg_zub00@mail.ru>
2022-05-24 13:42:02 +0300

    Проверить контакт

--- client/components/Agencies.vue
+++ client/components/Agencies.vue
@@ -6,7 +6,35 @@
         :items="data"
         :items-per-page="10"
         class="elevation-1"
-      ></v-data-table>
+      >
+        <template v-slot:item.Toggle="{ item }">
+          <v-sheet style="overflow: hidden">
+            <v-container class="fill-height">
+              <v-row style="justify-content: center;">
+                <v-btn width="100%" @click.stop="drawer = !drawer">
+                  Показать
+                </v-btn>
+              </v-row>
+            </v-container>
+            <v-navigation-drawer
+              width="800"
+              v-model="drawer"
+              absolute temporary center right
+            >
+              <div>
+                <div>
+                  <v-btn>Проверить контакт</v-btn>
+                </div>
+                <div>
+                  <h2>
+                    Мамонты (ой, контакты)
+                  </h2>
+                </div>
+              </div>
+            </v-navigation-drawer>
+          </v-sheet>
+        </template>
+      </v-data-table>
     </template>
   </div>
 </template>
@@ -15,18 +43,17 @@ import {Component, Prop, Vue} from "vue-property-decorator";
 @Component
 export default class Agencies extends Vue {
   @Prop () data!:any
+  drawer:boolean=false
   headers:any = [
-    { text: 'Id', value: '_id' },
     { text: 'Название организации', value: 'name'},
     { text: 'ФИО Директора', value: 'director'},
-    { text: 'Телефон организации', value: 'phone'},
+    { text: 'Телефон агентства', value: 'phone'},
     { text: 'E-mail', value: 'email'},
     { text: 'Комментарий', value: 'comment'},
     { text: 'Контактное лицо', value: 'contactname'},
     { text: 'Контактный телефон', value: 'contactphone'},
-    { text: 'Дата создания', value: 'createdAt'},
+    { text: 'Привязанные контакты', value: 'Toggle'},
   ]
-
 }
 </script>
 
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,7 +2,49 @@
   <div>
     <v-container style="max-width: 1500px !important; margin-top: 40px;">
       <div v-if="data">
-        <h2 style="margin: 40px">Агенства</h2>
+        <v-row style="margin: 40px">
+          <v-col>
+            <h2>Агенства</h2>
+          </v-col>
+          <v-col class="col-auto" data-app>
+            <template>
+              <div class="text-center">
+                <v-dialog
+                  width="500"
+                  v-model="dialog"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      v-on="on"
+                      v-bind="attrs"
+                    >
+                      Проверить контакт
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title class="text-h5 lighten-2">
+                    Проверить контакт
+                    </v-card-title>
+                    <v-card-text>
+                      <v-text-field/>
+                    </v-card-text>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn text>
+                       Проверить
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </div>
+            </template>
+
+          </v-col>
+          <v-col class="col-auto">
+            <v-btn href>Добавить агенство</v-btn>
+          </v-col>
+        </v-row>
         <Agencies :data="data"/>
       </div>
       <div v-else>Подождите...</div>
@@ -14,6 +56,7 @@ import {Component, Vue, Watch} from "vue-property-decorator"
 @Component
 export default class Index extends Vue {
   private data:any = ''
+  private dialog:boolean=false
 
   created () {
     if (!this.$restAuthData?.accessToken) {
